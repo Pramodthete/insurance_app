e79b5b5f2b98c1bc877a5186015ad2d03dd61f7a
Suraj <psuraj31413@gmail.com>
2024-07-20 17:16:54 +0530

    [Suraj] Add: Added prime vue library and setup configuration

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,19 @@ import App from './App.vue'
 import router from './router'
 import Vueform from '@vueform/vueform'
 import vueformConfig from '../vueform.config'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 app.use(Vueform, vueformConfig)
 app.use(createPinia())
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
 app.use(router)
 
 app.mount('#app')
